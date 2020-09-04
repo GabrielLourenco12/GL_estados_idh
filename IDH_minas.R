1757229365e6114490c78c73b0a1b84644b63e3f
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2020-09-03 21:17:57 -0300

    Limpando a memória principal

---
+++ IDH_minas.R
@@ -0,0 +1,14 @@
+##################################################################
+## script para plotar mapas estaduais de IDHM usando valores    ##
+## para cada município                                          ##                                                              ##
+##                                                              ##
+## desenvolvido por: Gabriel Louren?o                           ##
+## contato: gabriel9lourenco@gmail.com                          ##
+## linkedin: @gabriellourenco12                                 ##
+##################################################################
+
+#Removendo objetos armazenados na memória principal
+rm(list = ls())
+
+
+
