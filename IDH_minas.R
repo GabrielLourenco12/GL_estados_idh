f1d5cc72d80a9b2714e7f38a4a848d5a35c097d1
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2020-09-03 21:20:28 -0300

    Baixando as livrarias necessárias

--- IDH_minas.R
+++ IDH_minas.R
@@ -1,6 +1,6 @@
 ##################################################################
 ## script para plotar mapas estaduais de IDHM usando valores    ##
-## para cada município                                          ##                                                              ##
+## para cada município                                          ##                                                              
 ##                                                              ##
 ## desenvolvido por: Gabriel Louren?o                           ##
 ## contato: gabriel9lourenco@gmail.com                          ##
@@ -11,4 +11,15 @@
 rm(list = ls())
 
 
+####################################################
+## Carregando os pacotes                         ###
+##                                               ###
+####################################################
+
+library(geobr) #Para acessar os dados do IBGE
+library(ggplot2) #Para plotar os dados
+library(readxl)  #Para ler os dados do excel
+library(dplyr)   #Para processar os dados
+library(RColorBrewer)#Fornece paleta de cores
+library(ggspatial) # Para inserir a escala e a orientação do mapa
 
