cf6990a2b73956210ac9aa56f122b6da5f379475
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2020-09-03 21:25:47 -0300

    Lendo e tratando os dados de uma base do Excel do IBGE para o Brasil todo.

--- IDH_minas.R
+++ IDH_minas.R
@@ -23,3 +23,18 @@ library(dplyr)   #Para processar os dados
 library(RColorBrewer)#Fornece paleta de cores
 library(ggspatial) # Para inserir a escala e a orientação do mapa
 
+muni <- read_municipality(code_muni = 'MG') #Lendo os limites territoriais dos municípios de Minas Gerais
+mg <- read_state(code_state = "MG") #Lendo os limites territoriais do Estado de Minas Gerais
+
+#Definindo diretório de trabalho no meu Desktop
+setwd('E:/CURSOS/mapas_municipais/mapas_municipais/idh_minas/')
+
+dados <- read_excel('atlas2013_dadosbrutos_pt.xlsx',sheet = 2) %>% #Lendo os dados 
+  filter(ANO==2010 & UF==31) %>% #Selecionando linhas de dados para o ano de 2010
+  select(Codmun7,IDHM) %>%  #Selecionando coluna de dados
+  rename(code_muni=Codmun7) #Renomeando coluna de dados
+
+#Juntando os dados com base na coluna code_muni
+juntos <- inner_join(muni,dados,by="code_muni")
+
+
