2d1b9e36110d15cced0ddfebb40ef94cf78aa0c6
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2020-09-03 21:29:02 -0300

    Plotando os dados de uma forma bonita

--- IDH_minas.R
+++ IDH_minas.R
@@ -38,3 +38,19 @@ dados <- read_excel('atlas2013_dadosbrutos_pt.xlsx',sheet = 2) %>% #Lendo os dad
 juntos <- inner_join(muni,dados,by="code_muni")
 
 
+###########################################
+###           plotando os dados          ##
+###########################################
+
+ggplot(juntos)+ #Informando os dados a serem plotados
+  geom_sf(aes(fill=IDHM,col=IDHM))+ #Definindo as Cores (contorno e preenchimento dos munic?pios) em função do IDHM
+  geom_sf(data = mg,fill='transparent')+#Adicionando o contorno do Estado de Minas Gerais
+  scale_color_gradientn(colours = brewer.pal(9,'Spectral') )+ #Definindo escala de cores para o contorno dos municípios
+  scale_fill_gradientn(colours = brewer.pal(9,'Spectral'))+ #Definindo escala de cores para o preenchimento dos municípios
+  theme_minimal()+#Definindo o tema do gráfico
+  annotation_scale(location='bl')+#Inserindo a escala do mapa
+  annotation_north_arrow(location='tl', #Inserindo a orientação do mapa
+                         style = north_arrow_nautical(), #Inserindo o símbolo da orientação
+                         width = unit(1.8,'cm'), #Largura do símbolo
+                         height = unit(1.8,'cm'))+#Altura do símbolo
+  labs(title = "IDH Médio Para o Estado de Minas Gerais") #Definindo o título do mapa
